54dd93da1b8ccbd3cd40d3fc7a083d4158fff8ef
jimo <jimolonely@yahoo.com>
2019-01-06 16:46:56 +0800

    modify

--- src/main/java/com/jimo/algo/Solution.java
+++ src/main/java/com/jimo/algo/Solution.java
@@ -1114,4 +1114,50 @@ public class Solution {
 		}
 		return dp[nums.length - 1] >= nums.length - 1;
 	}
+
+	public int uniquePaths(int m, int n) {
+		int[] dp = new int[n + 1];
+		Arrays.fill(dp, 1);
+		for (int i = 2; i <= m; i++) {
+			for (int j = 2; j <= n; j++) {
+				dp[j] += dp[j - 1];
+			}
+		}
+		return dp[n];
+	}
+
+	public int coinChange(int[] coins, int amount) {
+		if (amount == 0 || coins == null || coins.length == 0) {
+			return 0;
+		}
+		int[] dp = new int[amount + 1];
+		Arrays.fill(dp, amount + 1);
+		dp[0] = 0;
+
+		for (int coin : coins) {
+			for (int i = coin; i <= amount; i++) {
+				dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+			}
+		}
+		return dp[amount] > amount ? -1 : dp[amount];
+	}
+
+	public int lengthOfLIS(int[] nums) {
+		if (nums.length == 0) {
+			return 0;
+		}
+		int maxLen = 1;
+		// 到第i个数时的最大上升长度
+		int[] dp = new int[nums.length];
+		Arrays.fill(dp, 1);
+		for (int i = 0; i < nums.length; i++) {
+			for (int j = 0; j < i; j++) {
+				if (nums[i] > nums[j]) {
+					dp[i] = Math.max(dp[i], dp[j] + 1);
+				}
+			}
+			maxLen = Math.max(maxLen, dp[i]);
+		}
+		return maxLen;
+	}
 }
