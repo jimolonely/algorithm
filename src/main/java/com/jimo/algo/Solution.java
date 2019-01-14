23a8b294d57a73e31a10c4c2b6acaa7937ab1e6e
jimo <jimolonely@yahoo.com>
2019-01-14 08:46:43 +0800

    add

--- src/main/java/com/jimo/algo/Solution.java
+++ src/main/java/com/jimo/algo/Solution.java
@@ -1359,4 +1359,55 @@ public class Solution {
 		}
 		return re;
 	}
+
+	public int findKthLargest(int[] nums, int k) {
+		Arrays.sort(nums);
+		return nums[nums.length - k];
+	}
+
+	public int findPeakElement(int[] nums) {
+		int left = 0, right = nums.length - 1;
+		while (left < right) {
+			int mid = left / 2 + right / 2;
+			if (mid > 0 && nums[mid] < nums[mid - 1]) {
+				right = mid - 1;
+			} else if (nums[mid] < nums[mid + 1]) {
+				left = mid + 1;
+			} else {
+				return mid;
+			}
+		}
+		return right;
+	}
+
+	public int[] searchRange(int[] nums, int target) {
+		int left = 0, right = nums.length - 1;
+		int i = -1;
+		while (left <= right) {
+			int mid = left + (right - left) / 2;
+			if (nums[mid] == target) {
+				i = mid;
+				break;
+			} else if (nums[mid] < target) {
+				left = mid + 1;
+			} else {
+				right = mid - 1;
+			}
+		}
+		if (i == -1) {
+			return new int[]{-1, -1};
+		}
+		int i1 = i, i2 = i;
+		left = i - 1;
+		right = i;
+		while (left >= 0 && nums[left] == target) {
+			i1 = left;
+			left--;
+		}
+		while (right < nums.length && nums[right] == target) {
+			i2 = right;
+			right++;
+		}
+		return new int[]{i1, i2};
+	}
 }
