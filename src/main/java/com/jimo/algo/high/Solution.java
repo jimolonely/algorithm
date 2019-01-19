9f0fa3cb5593cad8709ac31363918b182ad39e79
jimo <jimolonely@yahoo.com>
2019-01-19 12:23:56 +0800

    add 2

--- src/main/java/com/jimo/algo/high/Solution.java
+++ src/main/java/com/jimo/algo/high/Solution.java
@@ -1,5 +1,8 @@
 package com.jimo.algo.high;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author jimo
  * @date 2019/1/19 11:44
@@ -23,5 +26,55 @@ public class Solution {
 			output[i] = pre[i] * post[i];
 		}
 		return output;
+	}
+
+	public List<Integer> spiralOrder(int[][] matrix) {
+		List<Integer> re = new ArrayList<Integer>();
+		if (matrix.length == 0) {
+			return re;
+		}
+		int up = 0, down = matrix.length - 1, left = 0, right = matrix[0].length;
+		// one line
+		if (down == 0) {
+			for (int i = 0; i < right; i++) {
+				re.add(matrix[0][i]);
+			}
+			return re;
+		}
+		// one column
+		if (right == 0) {
+			for (int i = 0; i < down; i++) {
+				re.add(matrix[i][0]);
+			}
+			return re;
+		}
+		while (up <= down) {
+			for (int i = left; i <= right; i++) {
+				re.add(matrix[up][i]);
+			}
+			if (up + 1 > down) {
+				break;
+			}
+			for (int i = up + 1; i <= down; i++) {
+				re.add(matrix[i][right]);
+			}
+			if (right - 1 < left) {
+				break;
+			}
+			for (int i = right - 1; i >= left; i--) {
+				re.add(matrix[down][i]);
+			}
+			if (down - 1 < up) {
+				break;
+			}
+			for (int i = down - 1; i > up; i--) {
+				re.add(matrix[i][left]);
+			}
+			up++;
+			down--;
+			left++;
+			right--;
+		}
+		return re;
 	}
 }
