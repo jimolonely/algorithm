3a4a4f4186f62640c5efcd124fdd43d5fc9b21fe
jimo <jimolonely@yahoo.com>
2019-03-20 09:06:17 +0800

    add MyStack

--- src/main/java/com/jimo/algo/low/Solution2.java
+++ src/main/java/com/jimo/algo/low/Solution2.java
@@ -189,4 +189,51 @@ public class Solution2 {
         }
         return false;
     }
+
+    class MyStack {
+
+        private Queue<Integer> q;
+        private int size;
+
+        /**
+         * Initialize your data structure here.
+         */
+        public MyStack() {
+            size = 0;
+            q = new LinkedList<>();
+        }
+
+        /**
+         * Push element x onto stack.
+         */
+        public void push(int x) {
+            q.offer(x);
+            for (int i = 0; i < size; i++) {
+                q.offer(q.poll());
+            }
+            size++;
+        }
+
+        /**
+         * Removes the element on top of the stack and returns that element.
+         */
+        public int pop() {
+            size--;
+            return q.poll();
+        }
+
+        /**
+         * Get the top element.
+         */
+        public int top() {
+            return q.peek();
+        }
+
+        /**
+         * Returns whether the stack is empty.
+         */
+        public boolean empty() {
+            return size == 0;
+        }
+    }
 }
