1628e050d73fb660fcb59e315b75c155555a3958
jimo <jimolonely@yahoo.com>
2019-03-21 08:13:44 +0800

    add invertTree

--- src/main/java/com/jimo/algo/low/Solution2.java
+++ src/main/java/com/jimo/algo/low/Solution2.java
@@ -236,4 +236,26 @@ public class Solution2 {
             return size == 0;
         }
     }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode left = root.left;
+        root.left = root.right;
+        root.right = left;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
 }
