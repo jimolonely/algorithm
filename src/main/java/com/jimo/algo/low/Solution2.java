5aa3cdacc116963934a8ce34e6dbf67baa6d8b0c
jimo <jimolonely@yahoo.com>
2019-03-21 08:19:16 +0800

    add isPowerOfTwo

--- src/main/java/com/jimo/algo/low/Solution2.java
+++ src/main/java/com/jimo/algo/low/Solution2.java
@@ -258,4 +258,11 @@ public class Solution2 {
         invertTree(root.right);
         return root;
     }
+
+    public boolean isPowerOfTwo(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        return (n & (n - 1)) == 0;
+    }
 }
