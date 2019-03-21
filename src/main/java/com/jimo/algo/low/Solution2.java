41cfa360e08ffad441c3a95b39f365894ebf7b38
jimo <jimolonely@yahoo.com>
2019-03-21 08:45:25 +0800

    add MyQueue

--- src/main/java/com/jimo/algo/low/Solution2.java
+++ src/main/java/com/jimo/algo/low/Solution2.java
@@ -265,4 +265,53 @@ public class Solution2 {
         }
         return (n & (n - 1)) == 0;
     }
+
+    class MyQueue {
+
+        private Stack<Integer> s;
+        private Stack<Integer> h;
+
+        /**
+         * Initialize your data structure here.
+         */
+        public MyQueue() {
+            s = new Stack<>();
+            h = new Stack<>();
+        }
+
+        /**
+         * Push element x to the back of queue.
+         */
+        public void push(int x) {
+            while (!s.isEmpty()) {
+                h.push(s.pop());
+            }
+            s.push(x);
+            while (!h.isEmpty()) {
+                s.push(h.pop());
+            }
+        }
+
+        /**
+         * Removes the element from in front of queue and returns that element.
+         */
+        public int pop() {
+            return s.pop();
+        }
+
+        /**
+         * Get the front element.
+         */
+        public int peek() {
+            return s.peek();
+        }
+
+        /**
+         * Returns whether the queue is empty.
+         */
+        public boolean empty() {
+            return s.isEmpty();
+        }
+    }
+
 }
