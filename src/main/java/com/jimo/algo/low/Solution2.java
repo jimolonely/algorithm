0c5617e2e6dda40209aea03faf729ee78ac7c585
jimo <jimolonely@yahoo.com>
2019-03-21 08:59:42 +0800

    add lowestCommonAncestor

--- src/main/java/com/jimo/algo/low/Solution2.java
+++ src/main/java/com/jimo/algo/low/Solution2.java
@@ -314,4 +314,15 @@ public class Solution2 {
         }
     }
 
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || q == null || p == null) {
+            return null;
+        }
+        if (root.val > Math.max(p.val, q.val)) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else if (root.val < Math.max(p.val, q.val)) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        return root;
+    }
 }
