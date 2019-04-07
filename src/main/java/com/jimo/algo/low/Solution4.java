abd42179197b2f7ab64b6247c0100a8d77ee85c0
jimo <jimolonely@yahoo.com>
2019-04-07 19:37:07 +0800

    add arrayPairSum

--- src/main/java/com/jimo/algo/low/Solution4.java
+++ src/main/java/com/jimo/algo/low/Solution4.java
@@ -1,5 +1,6 @@
 package com.jimo.algo.low;
 
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -30,4 +31,15 @@ public class Solution4 {
         }
         return 1 + max;
     }
+
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int sum = 0;
+        for (int i = 0; i < nums.length; i += 2) {
+            sum += nums[i];
+        }
+        return sum;
+    }
+
+
 }
