7a6e8cf801989b575927162ed982948ef7eb9387
jimo <jimolonely@yahoo.com>
2019-04-10 20:59:10 +0800

    add mergeTrees

--- src/main/java/com/jimo/algo/low/Solution4.java
+++ src/main/java/com/jimo/algo/low/Solution4.java
@@ -234,4 +234,14 @@ public class Solution4 {
             }
         }
     }
+
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) {
+            return null;
+        }
+        TreeNode t = new TreeNode((t1 == null ? 0 : t1.val) + (t2 == null ? 0 : t2.val));
+        t.left = mergeTrees(t1 == null ? null : t1.left, t2 == null ? null : t2.left);
+        t.right = mergeTrees(t1 == null ? null : t1.right, t2 == null ? null : t2.right);
+        return t;
+    }
 }
