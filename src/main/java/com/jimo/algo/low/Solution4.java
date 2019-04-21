0b55b7cb5dcacc1928da1b3ad8938eb742e82d7f
jimo <jimolonely@yahoo.com>
2019-04-21 10:33:40 +0800

    add search

--- src/main/java/com/jimo/algo/low/Solution4.java
+++ src/main/java/com/jimo/algo/low/Solution4.java
@@ -666,4 +666,19 @@ public class Solution4 {
             return q.peek();
         }
     }
+
+    public int search(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return -1;
+    }
 }
