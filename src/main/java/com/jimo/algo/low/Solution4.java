e57fa4bd4dbb70468248aa5b5e58da31623be49d
jimo <jimolonely@yahoo.com>
2019-05-12 17:16:35 +0800

    add largestTriangleArea

--- src/main/java/com/jimo/algo/low/Solution4.java
+++ src/main/java/com/jimo/algo/low/Solution4.java
@@ -984,4 +984,20 @@ public class Solution4 {
         }
         return re;
     }
+
+    public double largestTriangleArea(int[][] points) {
+        double max = 0d;
+        for (int i = 0; i < points.length; i++) {
+            for (int j = i + 1; j < points.length; j++) {
+                for (int k = j + 1; k < points.length; k++) {
+                    double a = Math.abs(
+                            (points[j][0] - points[i][0]) * (points[k][1] - points[i][1]) -
+                                    (points[k][0] - points[i][0]) * (points[j][1] - points[i][1])
+                    ) / 2.0;
+                    max = Math.max(max, a);
+                }
+            }
+        }
+        return max;
+    }
 }
