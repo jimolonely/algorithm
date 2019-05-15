3b11cca23d41527c77a2cd130ca51df09e33d958
jimo <jimolonely@yahoo.com>
2019-05-15 08:15:31 +0800

    add toGoatLatin

---
+++ src/main/java/com/jimo/algo/low/Solution5.java
@@ -0,0 +1,18 @@
+package com.jimo.algo.low;
+
+/**
+ * @author jimo
+ * @date 19-5-15 上午8:02
+ */
+public class Solution5 {
+
+    public String toGoatLatin(String S) {
+        String[] s = S.split(" ");
+        for (int i = 0; i < s.length; i++) {
+            String fc = s[i].substring(0, 1);
+        }
+        return "";
+    }
+
+
+}
