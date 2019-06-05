fc321082dca2d0eb826ef60b116c6f33b5afb9d0
jimo <jimolonely@yahoo.com>
2019-06-05 08:16:58 +0800

    add binaryGap

--- src/main/java/com/jimo/algo/low/Solution5.java
+++ src/main/java/com/jimo/algo/low/Solution5.java
@@ -177,4 +177,18 @@ public class Solution5 {
         }
         return 0;
     }
+
+    public int binaryGap(int N) {
+        int max = 0;
+        int lastIndex = -1;
+        for (int i = 0; i < 32; i++) {
+            if (((N >> i) & 1) > 0) {
+                if (lastIndex > -1) {
+                    max = Math.max(max, i - lastIndex);
+                }
+                lastIndex = i;
+            }
+        }
+        return max;
+    }
 }
