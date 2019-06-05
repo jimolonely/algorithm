9ef5e598ef731d766c30a6c3ff934942397f0df8
jimo <jimolonely@yahoo.com>
2019-06-05 08:25:18 +0800

    add leafSimilar

--- src/main/java/com/jimo/algo/low/Solution5.java
+++ src/main/java/com/jimo/algo/low/Solution5.java
@@ -191,4 +191,43 @@ public class Solution5 {
         }
         return max;
     }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        List<Integer> l1 = new ArrayList<>();
+        List<Integer> l2 = new ArrayList<>();
+
+        inorder(root1, l1);
+        inorder(root2, l2);
+
+        if (l1.size() != l2.size()) {
+            return false;
+        }
+        for (int i = 0; i < l1.size(); i++) {
+            if (!l1.get(i).equals(l2.get(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void inorder(TreeNode root, List<Integer> l) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left, l);
+        if (root.left == null && root.right == null) {
+            l.add(root.val);
+        }
+        inorder(root.right, l);
+    }
 }
