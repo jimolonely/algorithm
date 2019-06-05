b99b1b336aa02fe5defbeb68bac614a7e47ce207
jimo <jimolonely@yahoo.com>
2019-06-05 09:11:12 +0800

    add projectionArea

--- src/main/java/com/jimo/algo/low/Solution5.java
+++ src/main/java/com/jimo/algo/low/Solution5.java
@@ -248,4 +248,22 @@ public class Solution5 {
         }
         return slow;
     }
+
+    public int projectionArea(int[][] grid) {
+        int n = grid.length;
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            // xy
+            int xz = 0, yz = 0, xy = 0;
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] > 0) {
+                    xy++;
+                }
+                xz = Math.max(grid[i][j], xz);
+                yz = Math.max(grid[j][i], yz);
+            }
+            sum += xz + yz + xy;
+        }
+        return sum;
+    }
 }
