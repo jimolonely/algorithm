521ac7ebcf72d4579a30ce49b39ac35dfa4961f2
jimo <jimolonely@yahoo.com>
2019-06-10 08:25:31 +0800

    add sortArrayByParity

--- src/main/java/com/jimo/algo/low/Solution5.java
+++ src/main/java/com/jimo/algo/low/Solution5.java
@@ -385,4 +385,20 @@ public class Solution5 {
         head = head.right;
         rebuild(root.right);
     }
+
+    public int[] sortArrayByParity(int[] A) {
+        int left = 0, right = A.length - 1;
+        while (left < right) {
+            if (A[left] % 2 == 1 && A[right] % 2 == 0) {
+                int t = A[left];
+                A[left] = A[right];
+                A[right] = t;
+            } else if (A[left] % 2 == 0) {
+                left++;
+            } else if (A[right] % 2 == 1) {
+                right--;
+            }
+        }
+        return A;
+    }
 }
